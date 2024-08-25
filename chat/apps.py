84c6dc571037fd9dd073a20d44731c49c9a5458a
kiruukiran232 <kirankumardevarapalli232@gmail.com>
2024-08-25 14:40:44 +0530

    Update apps.py

--- chat/apps.py
+++ chat/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
 
+class RegistrationConfig(AppConfig):
+    name = 'registration'
+
+    
 class ChatConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
